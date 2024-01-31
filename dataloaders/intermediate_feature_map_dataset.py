c91c76fce45d51629f56a3758aa876060223707e
Jasper Toussaint <jato00001@stud.uni-saarland.de>
2024-01-31 17:21:21 +0100

    update

--- dataloaders/intermediate_feature_map_dataset.py
+++ dataloaders/intermediate_feature_map_dataset.py
@@ -1,32 +1,26 @@
 from torch.utils.data import Dataset
-import os
 
 from utils import load_feature_map, get_file_path
 
 class IntermediateActivationsDataset(Dataset):
     def __init__(self, layer_name, original_activations_folder_path, train_dataset_length, params):
-        self.layer_name = layer_name
-        self.original_activations_folder_path = original_activations_folder_path
         self.train_dataset_length = train_dataset_length
         self.image_size = None
-        self.params = params
+
+        file_path = get_file_path(folder_path=original_activations_folder_path,  
+                                    layer_name=layer_name, 
+                                    params=params, 
+                                    file_name='activations.h5')
+        self.combined_activations = load_feature_map(file_path).float()
+        self.image_size = self.combined_activations.shape[1:]
 
     def __len__(self):
         return self.train_dataset_length
 
     def __getitem__(self, sample_idx):
-        file_path = get_file_path(self.original_activations_folder_path,  
-                                  layer_name=self.layer_name, 
-                                  params=self.params, 
-                                  file_name='activations.h5')
-        combined_activations = load_feature_map(file_path).float()
-        if sample_idx == 1:
-            self.image_size = combined_activations.shape[1:]
-        #print(f"Intermediate activation shape: {combined_activations[sample_idx].shape}")
-        return combined_activations[sample_idx]
+        #print(f"Intermediate activation shape: {self.combined_activations[sample_idx].shape}")
+        return self.combined_activations[sample_idx]
     
     def get_image_size(self):
-        if self.image_size is None:
-            self.__getitem__(sample_idx=1)
         #print(f"Image size: {self.image_size}")
         return tuple(self.image_size)
